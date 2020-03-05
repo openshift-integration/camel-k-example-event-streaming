e564aeb34de376416944152d420ea8ed11e2bcad
Otavio Rodolfo Piske <opiske@redhat.com>
2020-03-05 15:04:29 +0100

    Added documentation
    Includes:
    
    - Added documentation about installing the AMQ Broker instance
    - Added Camel-K installation steps
    - Added operator group for AMQ Streams subscription
    - Fixed invalid kind for ConfigMap configuration
    - Added documentation about configuring the demo
    - Fixed demo configuration steps
    - Added some commands for requirements verification
    - Fixed invalid subscription
    - Added documentation about deploying AMQ Streams
    - Removed external route definition because it's not used
    - Added additional details about VS Code extention requirements

--- audit-gatekeeper/src/main/java/GateKeeper.java
+++ audit-gatekeeper/src/main/java/GateKeeper.java
@@ -6,6 +6,11 @@ public class GateKeeper extends RouteBuilder {
     private static final Logger LOG = LoggerFactory.getLogger(GateKeeper.class);
 
     public void configure() throws Exception {
+        /**
+         * This leverages the Knative channels and decouples the route from the
+         * transport mechanism used to send the events. By using this, it is possible
+         * to use features like scale-to-zero from Knative.
+         */
         from("knative:channel/audit")
                 .streamCaching()
                 .convertBodyTo(String.class)
--- front-end/src/main/resources/site/js/index.js
+++ front-end/src/main/resources/site/js/index.js
@@ -42,7 +42,7 @@ function renderTimelineInfoIncident(obj) {
 $(document).ready(function () {
     var path = $('[timeline]').attr('data-api')
 
-    var url = "http://timeline-bridge.camel-k-event-streaming-dev.apps.camel-k.rhmw-integrations.net" + path;
+    var url = "http://timeline-bridge.camel-k-event-streaming-dev.my.host.net" + path;
 
     axios.get(url).then(function (response) {
         // console.log("Received " + response.data);
