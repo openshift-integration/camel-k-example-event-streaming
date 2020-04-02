51c543484a15e8bdd80f1191c77a026a4a3b668e
Jan <jbouska@redhat.com>
2020-04-02 13:14:37 +0200

    [ADD] Add GateKeeper test

--- audit-gatekeeper/GateKeeper.java
+++ audit-gatekeeper/GateKeeper.java
@@ -1,11 +1,8 @@
 // camel-k: language=java
 
 import org.apache.camel.builder.RouteBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class GateKeeper extends RouteBuilder {
-    private static final Logger LOG = LoggerFactory.getLogger(GateKeeper.class);
 
     public void configure() throws Exception {
         /**
@@ -14,10 +11,6 @@ public class GateKeeper extends RouteBuilder {
          * to use features like scale-to-zero from Knative.
          */
         from("knative:channel/audit")
-                .convertBodyTo(String.class)
-                .wireTap("direct:log");
-
-        from("direct:log")
-                .to("log:info");
+                .to("log:info?multiline=true&showBodyType=false");
     }
 }
