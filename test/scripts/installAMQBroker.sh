43e59580f04d739f6030e35947215c35c7513e0d
Jan <jbouska@redhat.com>
2020-05-06 18:12:12 +0200

    [Fix] Fix installation scripts

--- test/scripts/installAMQBroker.sh
+++ test/scripts/installAMQBroker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 function waitFor() {
-  for i in {1..20}; do
+  for i in {1..30}; do
     sleep 5
-    "$@" &> /dev/null && return
+    ("$@") && return
+    echo "$i Waiting for exit code of command \"$@\"."
   done
+  exit 1
 }
 
 TIMEOUT=${TIMEOUT:-30} 
@@ -18,18 +20,12 @@ sed "s/YAKS_NAMESPACE/${YAKS_NAMESPACE}/" "${SOURCE}"/resources/operatorGroup.ya
 oc create -f "${SOURCE}"/resources/amq-broker-subscription.yaml -n ${YAKS_NAMESPACE}
 
 #ensure operator pod is deployed and Ready
-waitFor oc get pod -l name=amq-broker-operator -n ${YAKS_NAMESPACE}
-oc wait pod -l name=amq-broker-operator --for condition=Ready -n ${YAKS_NAMESPACE}
+waitFor oc wait pod -l name=amq-broker-operator --for condition=Ready -n ${YAKS_NAMESPACE}
 
 #create broker
 oc create -f "${INFRA}"/messaging/broker/instances/amq-broker-instance.yaml -n ${YAKS_NAMESPACE}
-waitFor oc get pod -l ActiveMQArtemis=broker -n ${YAKS_NAMESPACE}
-oc wait pod -l ActiveMQArtemis=broker --for condition=Ready --timeout=600s -n ${YAKS_NAMESPACE}
-
-waitFor [[ `oc get ActiveMQArtemis broker -o=jsonpath='{.status.podStatus.ready}' -n ${YAKS_NAMESPACE}` !=  "" ]]
+waitFor oc wait pod -l ActiveMQArtemis=broker --for condition=Ready --timeout=600s -n ${YAKS_NAMESPACE}
 
 sleep $TIMEOUT
 #install addresses
 oc apply -f "${INFRA}"/messaging/broker/instances/addresses -n ${YAKS_NAMESPACE}
-
-
