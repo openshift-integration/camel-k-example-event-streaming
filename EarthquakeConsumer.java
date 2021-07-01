5c84856e6ba30db6a33876ae07778bfd28cde35c
Aurélien Pupier <apupier@redhat.com>
2021-07-01 10:58:35 +0200

    Update to new API property=file: instead of deprecated property-file= #51
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- EarthquakeConsumer.java
+++ EarthquakeConsumer.java
@@ -1,4 +1,4 @@
-// camel-k: language=java property-file=application.properties
+// camel-k: language=java property=file:application.properties
 // camel-k: dependency=github:openshift-integration:camel-k-example-event-streaming
 
 import java.util.ArrayList;
