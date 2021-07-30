aa3be8fbc5804b4a214555e017ed7eba5dc2fb98
Christoph Deppisch <cdeppisch@redhat.com>
2021-07-30 17:20:18 +0200

    fix(ENTESB-17058): Check for existing subscriptions on cluster in pre scripts
    Need to check that there is no global AMQ Streams OLM subscription already available on the cluster. Adding a 2nd subscription will fail the YAKS prepare test scripts.
    
    Make sure to only add the subscription when no global one is present on the cluster.
    
    (cherry picked from commit 7aecd9a1f3bcf58e702d69a5d0162752aa6c8206)

--- test/scripts/installAMQStreams.sh
+++ test/scripts/installAMQStreams.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 function waitFor() {
   for i in {1..30}; do
     sleep 5
@@ -9,20 +11,29 @@ function waitFor() {
   exit 1
 }
 
+AMQ_STREAMS_VERSION="v1.7.2"
 TIMEOUT=${TIMEOUT:-30}
 SOURCE=$(dirname "${BASH_SOURCE[0]}")
 INFRA="${SOURCE}"/../../infra
 
-#create OperatorGroup
-sed "s/YAKS_NAMESPACE/${YAKS_NAMESPACE}/" "${SOURCE}"/resources/operatorGroup.yaml | oc create -f - 2> /dev/null || echo "OperatorGroup already exists"
+CSV=$(oc get csv amqstreams.${AMQ_STREAMS_VERSION} -n ${YAKS_NAMESPACE} || echo "ERROR: failed to find AMQ Streams CSV")
+#check for existing amq-streams subscription
+if [ "${CSV//ERROR/}" != "${CSV}" ]; then
+  echo "Create AMQ Streams subscription"
 
-#install amq streams using OLM
-oc create -f "${SOURCE}"/resources/amq-streams-subscription.yaml -n ${YAKS_NAMESPACE}
+  #create OperatorGroup
+  sed "s/YAKS_NAMESPACE/${YAKS_NAMESPACE}/" "${SOURCE}"/resources/operatorGroup.yaml | oc create -f - 2> /dev/null || echo "OperatorGroup already exists"
 
-#ensure operator pod is deployed and Ready
-waitFor oc wait pod -l name=amq-streams-cluster-operator --for condition=Ready --timeout=60s -n ${YAKS_NAMESPACE}
+  #install AMQ streams using OLM
+  oc create -f "${SOURCE}"/resources/amq-streams-subscription.yaml -n ${YAKS_NAMESPACE}
 
-#create Kafka
+  #ensure operator pod is deployed and Ready
+  waitFor oc wait pod -l name=amq-streams-cluster-operator --for condition=Ready --timeout=60s -n ${YAKS_NAMESPACE}
+else
+  echo "AMQ Streams subscription already exists"
+fi
+
+#create Kafka cluster
 oc create -f "${INFRA}"/kafka/clusters/event-streaming-cluster.yaml -n ${YAKS_NAMESPACE}
 oc wait kafka/event-streaming-kafka-cluster --for=condition=Ready --timeout=600s -n ${YAKS_NAMESPACE}
 
