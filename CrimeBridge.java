4229be70f6096d2b888ed42e938d5de94e44750d
mmajerni <mmajerni@redhat.com>
2023-01-12 14:05:19 +0100

    Bump versions to new 1.10.x branch

--- CrimeBridge.java
+++ CrimeBridge.java
@@ -1,6 +1,6 @@
 // camel-k: language=java property=file:application.properties
 // camel-k: dependency=mvn:org.amqphub.quarkus:quarkus-qpid-jms
-// camel-k: dependency=github:openshift-integration:camel-k-example-event-streaming:1.8.x-SNAPSHOT
+// camel-k: dependency=github:openshift-integration:camel-k-example-event-streaming:1.10.x-SNAPSHOT
 
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
