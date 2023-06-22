c86648191eddc51dd843cb656991514eeb44a318
mmajerni <mmajerni@redhat.com>
2023-06-22 09:55:55 +0200

    PollutionData add new parameters

--- model/src/main/java/com/redhat/integration/pollution/PollutionData.java
+++ model/src/main/java/com/redhat/integration/pollution/PollutionData.java
@@ -41,6 +41,7 @@ public class PollutionData {
         }
     }
 
+    private String locationId;
     private String location;
     private String parameter;
     private DateInfo date;
@@ -49,6 +50,17 @@ public class PollutionData {
     private Coordinates coordinates;
     private String country;
     private String city;
+    private Boolean isMobile;
+    private Boolean isAnalysis;
+    private String entity;
+    private String sensorType;
+
+    public String getLocationId() {
+        return locationId;
+    }
+    public void setLocationId(String locationId) {
+        this.locationId = locationId;
+    }
 
     public String getLocation() {
         return location;
@@ -112,5 +124,37 @@ public class PollutionData {
 
     public void setCity(String city) {
         this.city = city;
+    }
+
+    public Boolean getIsMobile() {
+        return isMobile;
+    }
+
+    public void setIsMobile(Boolean isMobile) {
+        this.isMobile = isMobile;
+    }
+
+    public Boolean getIsAnalysis() {
+        return isAnalysis;
+    }
+
+    public void setIsAnalysis(Boolean isAnalysis) {
+        this.isAnalysis = isAnalysis;
+    }
+
+    public String getEntity() {
+        return entity;
+    }
+
+    public void setEntity(String entity) {
+        this.entity = entity;
+    }
+
+    public String getSensorType() {
+        return sensorType;
+    }
+
+    public void setSensorType(String sensorType) {
+        this.sensorType = sensorType;
     }
 }
